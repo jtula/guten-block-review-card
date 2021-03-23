b5af5c566509ddbe72029a9a889206fcb6424000
Jose A Tula <joseph.tula@gmail.com>
2021-03-23 09:54:48 -0400

    feat: add styles

--- src/block/block.js
+++ src/block/block.js
@@ -1,99 +1,116 @@
 /**
  * BLOCK: review-card
  *
- * Registering a basic block with Gutenberg.
- * Simple block, renders and saves the same content without any interactivity.
+ * based on Andy Bell series: "Learning Gutenberg"
+ * https://css-tricks.com/learning-gutenberg-7-building-our-block-custom-card-block/
+ *
  */
 
 //  Import CSS.
-import './editor.scss';
-import './style.scss';
+import "./editor.scss";
+import "./style.scss";
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { RichText, MediaUpload, PlainText } = wp.blockEditor;
+const { registerBlockType } = wp.blocks;
+const { Button } = wp.components;
 
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
-registerBlockType( 'cgb/block-review-card', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'review-card - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
-	keywords: [
-		__( 'review-card — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
-	],
+registerBlockType("review-card/main", {
+	title: "Card",
+	icon: "heart",
+	category: "common",
+	attributes: {
+		imageUrl: {
+			attribute: "src",
+			selector: ".card__image",
+		},
+		imageAlt: {
+			attribute: "alt",
+			selector: ".card__image",
+		},
+		title: {
+			source: "text",
+			selector: ".card__title",
+		},
+		body: {
+			type: "array",
+			source: "children",
+			selector: ".card__body",
+		},
+	},
+	edit({ attributes, className, setAttributes }) {
+		const getImageButton = (openEvent) => {
+			if (attributes.imageUrl) {
+				return (
+					<img
+						alt=""
+						aria-hidden="true"
+						src={attributes.imageUrl}
+						onClick={openEvent}
+						className="image"
+					/>
+				);
+			}
+			return (
+				<div className="button-container">
+					<Button onClick={openEvent} className="button button-large">
+						Pick an image
+					</Button>
+				</div>
+			);
+		};
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Component.
-	 */
-	edit: ( props ) => {
-		// Creates a <p class='wp-block-cgb-block-review-card'></p>.
 		return (
-			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>review-card</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+			<div className="container">
+				<MediaUpload
+					onSelect={(media) => {
+						setAttributes({ imageAlt: media.alt, imageUrl: media.url });
+					}}
+					type="image"
+					value={attributes.imageID}
+					render={({ open }) => getImageButton(open)}
+				/>
+				<PlainText
+					onChange={(content) => setAttributes({ title: content })}
+					value={attributes.title}
+					placeholder="Your card title"
+					className="heading"
+				/>
+				<RichText
+					onChange={(content) => setAttributes({ body: content })}
+					value={attributes.body}
+					multiline="p"
+					placeholder="Your card text"
+					allowedFormats={["bold", "italic", "underline"]}
+					isSelected={attributes.isSelected}
+				/>
 			</div>
 		);
 	},
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 *
-	 * @param {Object} props Props.
-	 * @returns {Mixed} JSX Frontend HTML.
-	 */
-	save: ( props ) => {
+	save({ attributes }) {
+		const cardImage = (src, alt) => {
+			if (!src) {
+				return null;
+			}
+
+			if (alt) {
+				return <img className="card__image" src={src} alt={alt} />;
+			}
+
+			// No alt set, so let's hide it from screen readers
+			return (
+				<img className="card__image" src={src} alt="" aria-hidden="true" />
+			);
+		};
+
 		return (
-			<div className={ props.className }>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>review-card</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+			<div className="card">
+				{cardImage(attributes.imageUrl, attributes.imageAlt)}
+				<div className="card__content">
+					<h3 className="card__title">{attributes.title}</h3>
+					<div className="card__body">{attributes.body}</div>
+				</div>
 			</div>
 		);
 	},
-} );
+});
